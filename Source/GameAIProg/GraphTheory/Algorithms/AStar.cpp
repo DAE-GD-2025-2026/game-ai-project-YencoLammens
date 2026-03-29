8ea21ec5e1f2057e654a4d4a34efaf9a0f1ddc4f
Yenco Lammens <161343595+YencoLammens@users.noreply.github.com>
2026-03-29 22:59:40 +0200

    A* implementation seemingly working, bugfixes might follow if I spot issues

--- Source/GameAIProg/GraphTheory/Algorithms/AStar.cpp
+++ Source/GameAIProg/GraphTheory/Algorithms/AStar.cpp
@@ -11,6 +11,74 @@ AStar::AStar(Graph* const pGraph, HeuristicFunctions::Heuristic hFunction)
 std::vector<Node*>AStar::FindPath(Node* const pStartNode, Node* const pGoalNode)
 {
 	std::vector<Node*> path{};
+	std::vector<NodeRecord> openList{};
+	std::vector<NodeRecord> closedList{};
+	NodeRecord currentRecord{};
+
+	NodeRecord startRecord{};
+	startRecord.pNode = pStartNode;
+	startRecord.pConnection = nullptr;
+	startRecord.costSoFar = 0.f;
+	startRecord.estimatedTotalCost = GetHeuristicCost(pStartNode, pGoalNode);
+	openList.push_back(startRecord);
+
+	while (!openList.empty())
+	{
+		currentRecord = *std::min_element(openList.begin(), openList.end());
+
+		if (currentRecord.pNode == pGoalNode)
+			break;
+
+		for (Connection* pConnection : pGraph->FindConnectionsFrom(currentRecord.pNode->GetId()))
+		{
+			Node* pNextNode = pGraph->GetNode(pConnection->GetToId()).get();
+			float newCostSoFar = currentRecord.costSoFar + pConnection->GetWeight();
+
+			auto closedIt = std::find_if(closedList.begin(), closedList.end(),
+				[pNextNode](NodeRecord const& Record){ return Record.pNode == pNextNode; });
+
+			if (closedIt != closedList.end())
+			{
+				if (closedIt->costSoFar <= newCostSoFar)
+					continue;
+				closedList.erase(closedIt);
+			}
+
+			auto openIt = std::find_if(openList.begin(), openList.end(),
+				[pNextNode](NodeRecord const& Record){ return Record.pNode == pNextNode; });
+
+			if (openIt != openList.end())
+			{
+				if (openIt->costSoFar <= newCostSoFar)
+					continue;
+				openList.erase(openIt);
+			}
+
+			NodeRecord newRecord{};
+			newRecord.pNode = pNextNode;
+			newRecord.pConnection = pConnection;
+			newRecord.costSoFar = newCostSoFar;
+			newRecord.estimatedTotalCost = newCostSoFar + GetHeuristicCost(pNextNode, pGoalNode);
+			openList.push_back(newRecord);
+		}
+
+		openList.erase(std::find(openList.begin(), openList.end(), currentRecord));
+		closedList.push_back(currentRecord);
+	}
+
+	if (currentRecord.pNode != pGoalNode)
+		return path;
+
+	while (currentRecord.pNode != pStartNode)
+	{
+		path.push_back(currentRecord.pNode);
+		auto prevIt = std::find_if(closedList.begin(), closedList.end(),
+			[&](NodeRecord const& Record){ return Record.pNode == pGraph->GetNode(currentRecord.pConnection->GetFromId()).get(); });
+		currentRecord = *prevIt;
+	}
+
+	path.push_back(pStartNode);
+	std::reverse(path.begin(), path.end());
 	return path;
 }
 
